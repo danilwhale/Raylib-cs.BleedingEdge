18e5d40147031cf496028262e070e1d4b5b4f13f
danil <61111955+danilwhale@users.noreply.github.com>
2025-05-16 18:18:36 +0300

    feat(raymath): make types equatable and add helper methods

--- Raylib-cs.BleedingEdge/Types/Raymath/Float16.cs
+++ Raylib-cs.BleedingEdge/Types/Raymath/Float16.cs
@@ -1,9 +1,53 @@
-using System.Runtime.InteropServices;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Raylib_cs.BleedingEdge;
 
-[StructLayout(LayoutKind.Sequential)]
-public unsafe struct Float16
+// column-major
+public struct Float16 : IEquatable<Float16>
 {
-    public fixed float Values[16];
+    public Vector4 X; // first column
+    public Vector4 Y; // second column
+    public Vector4 Z; // third column
+    public Vector4 W; // fourth column
+
+    /// <remarks>Matrix still will be in column-major order, make sure to transpose it.</remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Matrix4x4 AsMatrix4x4()
+    {
+        return Unsafe.ReadUnaligned<Matrix4x4>(ref Unsafe.As<Float16, byte>(ref this));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(Float16 other)
+    {
+        return X.Equals(other.X) &&
+               Y.Equals(other.Y) &&
+               Z.Equals(other.Z) &&
+               W.Equals(other.W);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object? obj)
+    {
+        return obj is Float16 other && Equals(other);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z, W);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Float16 left, Float16 right)
+    {
+        return left.Equals(right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Float16 left, Float16 right)
+    {
+        return !left.Equals(right);
+    }
 }
--- Raylib-cs.BleedingEdge/Types/Raymath/Float3.cs
+++ Raylib-cs.BleedingEdge/Types/Raymath/Float3.cs
@@ -1,9 +1,63 @@
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 
 namespace Raylib_cs.BleedingEdge;
 
 [StructLayout(LayoutKind.Sequential)]
-public unsafe struct Float3
+public struct Float3 : IEquatable<Float3>
 {
-    public fixed float Values[3];
+    public float X;
+    public float Y;
+    public float Z;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector128<float> AsVector128()
+    {
+        return new Vector4(X, Y, Z, 0.0f).AsVector128();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector3 AsVector3()
+    {
+        return new Vector3(X, Y, Z);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(Float3 other)
+    {
+        if (Vector128.IsHardwareAccelerated)
+        {
+            return AsVector128().Equals(other.AsVector128());
+        }
+        
+        return X.Equals(other.X) && 
+               Y.Equals(other.Y) && 
+               Z.Equals(other.Z);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object? obj)
+    {
+        return obj is Float3 other && Equals(other);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Float3 left, Float3 right)
+    {
+        return left.Equals(right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Float3 left, Float3 right)
+    {
+        return !left.Equals(right);
+    }
 }
